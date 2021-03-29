0c47e4b62b7181493c125c6ff69dddeeef0c2847
Francesco Musella <33572147+fmusella@users.noreply.github.com>
2021-03-29 18:21:00 +0200

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,16 @@
+// This header file contains the parameters used for the simulations of in-silico Hi-C, SPRITE and GAM on SBS model structures
+
+#ifndef SETTINGS_HEADER_H
+#define SETTINGS_HEADER_H
+
+extern const int N_structures;
+
+extern const int beads_number;
+extern const int beads_to_bins;
+extern const int bins_number;
+
+extern const double max_distance_to_link;
+extern const double R_cell;
+extern const double h;
+
+#endif
