25248d6fed4d25931792f5211bd812b01c4cb85d
Francesco Musella <33572147+fmusella@users.noreply.github.com>
2021-03-31 15:41:03 +0200

    Add files via upload

---
+++ settings.c
@@ -0,0 +1,11 @@
+#include "./settings.h"
+
+const int N_structures = 500;
+
+const int beads_number = 2250;
+const int beads_to_bins = 15;
+const int bins_number = 150;
+
+const double max_distance_to_link = 3.;
+const double R = 66.;
+const double h = 6.;
